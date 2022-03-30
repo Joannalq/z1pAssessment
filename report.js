5476421a3dbe37866f68b25d01fff48f063cd9dd
Joannalq <joanna851026@hotmail.com>
2022-03-30 23:45:44 +1100

    add report name with timestamp

--- report.js
+++ report.js
@@ -3,7 +3,7 @@ var reporter = require('cucumber-html-reporter');
 var options = {
   theme: 'bootstrap',
   jsonFile: './cucumber_report.json',
-  output: 'report/cucumber_report.html',
+  output: `report/cucumber_report_${new Date().getTime()}.html`,
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
   launchReport: true,
